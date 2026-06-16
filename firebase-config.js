fac7a96aaf1db04e6d9bcf2aefa5069a0ad0e96d
b.lam <b.lam@ggusd.us>
2026-06-16 12:46:29 -0700

    Add Firebase config and refactor to separate config file

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+export const firebaseConfig = {
+  apiKey:            "AIzaSyAXelJe7gT3GHXcY9vqKh3spt2Crjl3_nw",
+  authDomain:        "contest-dbbaa.firebaseapp.com",
+  databaseURL:       "https://contest-dbbaa-default-rtdb.firebaseio.com",
+  projectId:         "contest-dbbaa",
+  storageBucket:     "contest-dbbaa.firebasestorage.app",
+  messagingSenderId: "776205182944",
+  appId:             "1:776205182944:web:7ae5365cdf933fcadfad85",
+};
+
+export const CHILIS = [
+  { id: "1", label: "Chili #1" },
+  { id: "2", label: "Chili #2" },
+  { id: "3", label: "Chili #3" },
+  { id: "4", label: "Chili #4" },
+];
